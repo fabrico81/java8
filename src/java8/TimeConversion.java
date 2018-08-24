b005a3ee52c01dff023d48c51221050a8ca57117
Faber <fabrizio.corrente@gmail.com>
2018-08-24 17:35:42 +0200

    Time Conversion

---
+++ src/java8/TimeConversion.java
@@ -0,0 +1,18 @@
+package java8;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeConversion {
+
+    public static void main(String [] args){
+        String time = "12:00:00AM";
+        timeConverson(time);
+
+    }
+    private static void timeConverson(String time){
+
+        String result = DateTimeFormatter.ISO_TIME.format(LocalTime.parse(time, DateTimeFormatter.ofPattern("hh:mm:ssa"))).toString();
+        System.out.println("result = " + result);
+    }
+}
